c821fd9d9f8bfcddfbf4d5cf7652fb943e92d7d2
Yassine  Medah <yassinemedah1994@gmail.com>
2025-04-19 16:55:20 +0200

    path fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/strands-frontend-test/',
   plugins: [react()],
   test: {
     globals: true,
