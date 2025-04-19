55dce71953c2e00d7815438d81a2406696d7d3da
Yassine  Medah <yassinemedah1994@gmail.com>
2025-04-19 11:43:52 +0200

    display the data using recharts and style it with Scss and clean the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.scss";
+import BreedsChart from "./features/BreedsChart";
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className={`app-container ${darkMode ? "dark" : "light"}`}>
+        <button
+          className="toggle-button"
+          onClick={() => setDarkMode((prev) => !prev)}
+        >
+          {darkMode ? "☀️ Light Mode" : "🌙 Dark Mode"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h1>Top 10 Dog Breeds by Image Count</h1>
+        <BreedsChart />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
